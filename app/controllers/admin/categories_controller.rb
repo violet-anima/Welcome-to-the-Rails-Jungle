a0f4b7a02bb47eef647937bdb18fb8531ee430a5
violet-anima <violet_anima@zoho.com>
2017-06-09 01:24:50 +0000

    edited categories_controller.rb

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -11,7 +11,7 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :categories], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'New Category Has Been Created!'
     else
       render :new
     end
